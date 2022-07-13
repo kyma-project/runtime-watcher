636da701f2458db3a9a02b025adf06393753c8b3
khlifi411 <46890720+khlifi411@users.noreply.github.com>
2022-07-13 07:51:32 +0200

    removed cd step from chart generation script

--- operator/ocm/chart/watcher-chart-package.sh
+++ operator/ocm/chart/watcher-chart-package.sh
@@ -5,8 +5,7 @@ HELM_PACKAGE_PATH=$(pwd)
 HELM_CHART_PATH=$(pwd)/kyma-watcher
 
 # render kustomize manifests and copy them to templates folder for helm package
-cd "${MAKEFILE_PATH}"
-make helm-prepare
+make -C "${MAKEFILE_PATH}" helm-prepare
 
 # package helm chart
 helm package "${HELM_CHART_PATH}" -d "${HELM_PACKAGE_PATH}"
