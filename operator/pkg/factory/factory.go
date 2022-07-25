e92f051f0bbf94074b90d2ff3ebef0aa7d4f7700
Jeremy Harisch <jeremy.harisch@sap.com>
2022-07-25 17:33:36 +0200

    Watching now based on GVRs and having optional labels

--- operator/pkg/factory/factory.go
+++ operator/pkg/factory/factory.go
@@ -7,9 +7,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/dynamic/dynamicinformer"
-	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 	"strings"
@@ -30,38 +28,25 @@ func InformerFactoryWithLabel(client dynamic.Interface, mgr ctrl.Manager, label,
 	return informerFactory, err
 }
 
-func BuildInformerSet(gv schema.GroupVersion, resources *metav1.APIResourceList, informerFactory dynamicinformer.DynamicSharedInformerFactory) map[string]*source.Informer {
+func InformerFactory(client dynamic.Interface, mgr ctrl.Manager) (dynamicinformer.DynamicSharedInformerFactory, error) {
+	// Create informerFactory for each configured label
+	informerFactory := dynamicinformer.NewDynamicSharedInformerFactory(client, time.Minute*30)
+	err := mgr.Add(manager.RunnableFunc(
+		func(ctx context.Context) error {
+			informerFactory.Start(ctx.Done())
+			return nil
+		}))
+	return informerFactory, err
+}
+
+func BuildInformerSet(gvrList []schema.GroupVersionResource, informerFactory dynamicinformer.DynamicSharedInformerFactory) map[string]*source.Informer {
 	dynamicInformerSet := make(map[string]*source.Informer)
-	for _, resource := range resources.APIResources {
-		if strings.Contains(resource.Name, "/") || !strings.Contains(resource.Verbs.String(), "watch") {
+	for _, gvr := range gvrList {
+		if strings.Contains(gvr.Resource, "/") {
 			// Skip not listable resources, i.e. nodes/proxy
 			continue
 		}
-		// TODO have propper logging in place
-		//r.Logger.Info(fmt.Sprintf("Resource `%s` from GroupVersion `%s` will be watched", resource.Name, gv.String()))
-		gvr := gv.WithResource(resource.Name)
 		dynamicInformerSet[gvr.String()] = &source.Informer{Informer: informerFactory.ForResource(gvr).Informer()}
 	}
 	return dynamicInformerSet
-}
-
-func GetResourceList(mgr ctrl.Manager, gvs []schema.GroupVersion) (map[schema.GroupVersion]*metav1.APIResourceList, error) {
-	// Create K8s-Client
-	cs, err := kubernetes.NewForConfig(mgr.GetConfig())
-	if err != nil {
-		return nil, err
-	}
-	// Get Resources for configured GV
-	var resourcesMap = map[schema.GroupVersion]*metav1.APIResourceList{}
-	for _, gv := range gvs {
-		resources, err := cs.ServerResourcesForGroupVersion(gv.String())
-		if client.IgnoreNotFound(err) != nil {
-			return nil, err
-		}
-		// Resources found
-		if err == nil {
-			resourcesMap[gv] = resources
-		}
-	}
-	return resourcesMap, nil
 }
