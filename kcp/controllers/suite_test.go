669563337fe59de679028f0b8ec6306dbecd2f7a
Jeremy Harisch <jeremy.harisch@sap.com>
2022-08-05 11:14:02 +0200

    Fix testEnv lint

--- kcp/controllers/suite_test.go
+++ kcp/controllers/suite_test.go
@@ -53,7 +53,7 @@ var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
 	By("bootstrapping test environment")
-	testEnv := &envtest.Environment{
+	testEnv = &envtest.Environment{
 		CRDDirectoryPaths:     []string{filepath.Join("..", "config", "crd", "bases")},
 		ErrorIfCRDPathMissing: true,
 	}
