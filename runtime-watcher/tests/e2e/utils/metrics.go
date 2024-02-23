44d61d2d5259b6b8e04e118bd462afd11b2a9ede
Badr, Nesma <nesma.badr@sap.com>
2024-02-23 17:06:09 +0100

    Remove unnecessary logs

--- runtime-watcher/tests/e2e/utils/metrics.go
+++ runtime-watcher/tests/e2e/utils/metrics.go
@@ -5,25 +5,53 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	exec2 "os/exec"
 	"regexp"
 	"strconv"
+
+	"github.com/onsi/ginkgo/v2/dsl/core"
 
 	"github.com/kyma-project/runtime-watcher/skr/internal/watchermetrics"
 )
 
+func ExposeSKRMetricsEndpoint() error {
+	cmd := exec2.Command("kubectl", "config", "use-context", "k3d-skr")
+	if _, err := cmd.CombinedOutput(); err != nil {
+		return err
+	}
+
+	cmd = exec2.Command("kubectl", "patch", "svc", "skr-webhook-metrics", "-p",
+		"[{\"op\": \"replace\", \"path\": \"/spec/ports/0/nodePort\", \"value\":2112}]",
+		"--type=json", "-n", "kyma-system")
+	if _, err := cmd.CombinedOutput(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func GetWatcherRequestDurationMetric(ctx context.Context) (float64, error) {
 	metricsBody, err := getMetricsBody(ctx)
+	core.GinkgoWriter.Println(metricsBody)
+
 	if err != nil {
 		return 0, err
 	}
 
 	regex := regexp.MustCompile(watchermetrics.RequestDuration)
+	core.GinkgoWriter.Println(regex)
+
 	match := regex.FindStringSubmatch(metricsBody)
 	if len(match) < 1 {
+		core.GinkgoWriter.Println("METRIC NOT FOUND")
+
 		return 0, fmt.Errorf("metric %s not found", watchermetrics.RequestDuration)
 	}
 
 	duration, err := strconv.ParseFloat(match[1], 64)
+	core.GinkgoWriter.Println("------------------")
+
+	core.GinkgoWriter.Println(duration)
 	if err != nil {
 		return 0, fmt.Errorf("couldn't parse metric %s value", watchermetrics.RequestDuration)
 	}
@@ -32,7 +60,7 @@ func GetWatcherRequestDurationMetric(ctx context.Context) (float64, error) {
 
 func getMetricsBody(ctx context.Context) (string, error) {
 	clnt := &http.Client{}
-	request, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:9081/metrics", nil)
+	request, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:2112/metrics", nil)
 	if err != nil {
 		return "", fmt.Errorf("request to metrics endpoint :%w", err)
 	}
--- runtime-watcher/tests/e2e/watcher_metrics_test.go
+++ runtime-watcher/tests/e2e/watcher_metrics_test.go
@@ -21,11 +21,15 @@ var _ = Describe("Watcher Metrics", Ordered, func() {
 	}
 
 	Context("Given SKR Cluster", func() {
-		It("When Runtime Watcher deployment is ready", func() {
-			Eventually(deploymentReady).
-				WithContext(ctx).
-				WithArguments(runtimeClient, watcher).
-				Should(Succeed())
+		It("When Metrics Endpoint is exposed", func() {
+			Expect(ExposeSKRMetricsEndpoint()).To(Succeed())
+
+			By("And Runtime Watcher deployment is ready", func() {
+				Eventually(deploymentReady).
+					WithContext(ctx).
+					WithArguments(runtimeClient, watcher).
+					Should(Succeed())
+			})
 
 			By("And spec of SKR Kyma CR is changed", func() {
 				Eventually(changeRemoteKymaChannel).
