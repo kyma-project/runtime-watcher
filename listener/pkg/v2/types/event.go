bc4d0d4651f02badf74f70512d35af13f2b04f51
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2025-08-21 13:24:33 +0200

    Review fix

--- listener/pkg/v2/types/event.go
+++ listener/pkg/v2/types/event.go
@@ -2,7 +2,11 @@ package types
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
+
+// ObjectKey identifies a Kubernetes Object.
+type ObjectKey = types.NamespacedName
 
 type WatchEvent struct {
 	Owner      ObjectKey               `json:"owner"`
