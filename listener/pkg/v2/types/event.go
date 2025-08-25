2cf3f8cc5232368aa6c04246be372367b8237977
Raj <54686422+LeelaChacha@users.noreply.github.com>
2025-08-25 13:18:33 +0200

    feat: Add SkrMeta Type (#598)

--- listener/pkg/v2/types/event.go
+++ listener/pkg/v2/types/event.go
@@ -12,4 +12,5 @@ type WatchEvent struct {
 	Owner      ObjectKey               `json:"owner"`
 	Watched    ObjectKey               `json:"watched"`
 	WatchedGvk metav1.GroupVersionKind `json:"watchedGvk"`
+	SkrMeta    SkrMeta                 `json:"-"`
 }
